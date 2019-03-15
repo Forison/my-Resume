c95d615c2fe3a0ce6ad914ef9f76b19011f39016
forison <forison6205@gmail.com>
2019-03-14 20:08:07 -0700

    restyle twelve

--- js/navbar.js
+++ js/navbar.js
@@ -1,115 +1,28 @@
-/*$(document).ready(function(){
-//hide section three on window load 
-  //$('#sectionThree').hide();
-   $('#sectionThree').css('border','10px solid green');
+function animWho(){
 
-//activate scroll event 
+  $(window).scroll(function(){
 
- $(window).scroll(function(){
- $var myScroll = $(document).scrollTop();
-//condition of aninamtion
- if (myScroll =400) {
-   $('#sectionThree').addClass('zoomIn');
- }
- //end condition for animatio    
-});
-//end of scroll event 
-});
-*/
+
+  var scrollLevel=$(document).scrollTop();
+  var who=$("#left")
+
+  if (scrollLevel>=130) {
+   //show who left by adding a class
+   who.show("slow")
+  }else{
+    who.hide("slow")
+  }
+
+  })
+
+
+}
+
+
 $(document).ready(function(){
-   $('#sectionTwo').hide();
-   $('#ourMission').hide();
-   $('#ourVision').hide();
-   $('#ourCommittment').hide();
-   $('#ourCom').hide();
-   $('#sectionSeven').hide();
-   $('#eighLeft').hide();
-   $('#eighLeftOne').hide();
-   $('#eighLeftTwo').hide();
-     
 
-$(window).scroll(function(){
- //nav un animation point 
-      var winScroll=$(document).scrollTop();
-     if (winScroll >50) {
-      $('#sectionTwo').show('slow');
-     }else{
-      $('#sectionTwo').hide();
-     }
+  //hide who by default 
+  $("#left").hide()
 
-
-     if (winScroll >1300) {
-      $('#ourMission').show('slow');
-      }else{
-      $('#ourMission').hide();
-     }
-      // end of first if 
-
-      //beg second if 
-      if(winScroll >1300){
-          $('#ourVision').show('slow');  
-      }else{
-      $('#ourVision').hide();
-     }
-      //end of anoda if
-
-      //beg of if
-        if (winScroll >1200) {
-          $('#ourCommittment').show('slow');
-        }else{
-      $('#ourCommittment').hide();
-     }
-      //end of anoda
-
-      //beg of anoda
-          if (winScroll >1550) {
-            $('#ourCom').show('slow');
-      //end of anoda
-     }else{
-      $('#ourCom').hide();
-     }
-
-      //beg of anoda
-          if (winScroll >1050) {
-            $('#sectionSeven').show('slow');
-          }
-
-          if(winScroll>2000){
-            $('#eighLeft').show('slow'); 
-          }else{
-           $('#eighLeft').hide();
-          }
-          if(winScroll>2200){
-            $('#eighLeftOne').show('slow'); 
-          }else{
-            $('#eighLeftOne').hide();
-           }
-          if(winScroll>2500){
-            $('#eighLeftTwo').show('slow'); 
-          }else{
-           $('#eighLeftTwo').hide();
-          }
-      //end of anoda
-     //end of nuav Un nav bar
- //end condition for animatio    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
- particlesJS.load('footer','particles.json',function(){
-             console.log('particles loaded...');
-               });
-  });
+  animWho()
+})
